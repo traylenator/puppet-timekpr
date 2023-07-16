ee654aa773fad5efa4fb36a319d8b9a8937a4a0a
Steve Traylen <steve.traylen@cern.ch>
2023-07-16 22:00:06 +0200

    Better Defaults

--- spec/defines/user_spec.rb
+++ spec/defines/user_spec.rb
@@ -14,13 +14,14 @@ describe 'timekpr::user' do
 
         it {
           is_expected.to contain_file('/var/lib/timekpr/config/timekpr.steve.conf').
-            with_content(%r{^ALLOWED_HOURS_1 = $}).
-            with_content(%r{^ALLOWED_HOURS_7 = $})
+          with_content(%r{^ALLOWED_HOURS_1 = 0\[0-59\];1\[0-59\];2\[0-59\];3\[0-59\];4\[0-59\];5\[0-59\];6\[0-59\];7\[0-59\];8\[0-59\];9\[0-59\];10\[0-59\];11\[0-59\];13\[0-59\];15\[0-59\];16\[0-59\];17\[0-59\];18\[0-59\];19\[0-59\];20\[0-59\];21\[0-59\];23\[0-59\]$}).
+            with_content(%r{^ALLOWED_HOURS_7 = 0\[0-59\];1\[0-59\];2\[0-59\];3\[0-59\];4\[0-59\];5\[0-59\];6\[0-59\];7\[0-59\];8\[0-59\];9\[0-59\];10\[0-59\];11\[0-59\];13\[0-59\];15\[0-59\];16\[0-59\];17\[0-59\];18\[0-59\];19\[0-59\];20\[0-59\];21\[0-59\];23\[0-59\]$})
         }
 
         context 'with a day set' do
           let(:params) do
             {
+              defaults: {},
               days: {
                 monday:  { '3': [0, 45] },
                 tuesday: {
@@ -78,7 +79,7 @@ describe 'timekpr::user' do
           it {
             is_expected.to contain_file('/var/lib/timekpr/config/timekpr.steve.conf').
               with_content(%r{^ALLOWED_HOURS_1 = 3\[0-45\];6\[0-41\]$}).
-              with_content(%r{^ALLOWED_HOURS_2 = 3\[0-51\];6\[0-41\];21\[10-49]$}).
+              with_content(%r{^ALLOWED_HOURS_2 = 3\[0-51\];6\[0-41\];21\[10-49\]$}).
               with_content(%r{^ALLOWED_HOURS_7 = 3\[0-45\];6\[0-41\]$})
           }
         end
